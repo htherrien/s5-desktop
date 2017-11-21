ba76a134ee87b6ec64e5f445d79f56bf1c9f503f
theh2203 <theh2203@LAB4103-GEL04>
2017-11-21 17:32:47 -0500

    First commit

---
+++ mini-s-desktop/SerialPortManager.cs
@@ -0,0 +1,247 @@
+﻿// Copyright heiswayi nrird (https://heiswayi.github.io/2016/csharp-simple-serialport-singleton-class)
+// SerialPortManager.cs
+
+using System;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace mini_s_desktop
+{
+    public sealed class SerialPortManager
+    {
+        private static readonly Lazy<SerialPortManager> lazy = new Lazy<SerialPortManager>(() => new SerialPortManager());
+        public static SerialPortManager Instance { get { return lazy.Value; } }
+
+        private SerialPort _serialPort;
+        private Thread _readThread;
+        private volatile bool _keepReading;
+
+        private SerialPortManager()
+        {
+            _serialPort = new SerialPort();
+            _readThread = null;
+            _keepReading = false;
+        }
+
+        /// <summary>
+        /// Update the serial port status to the event subscriber
+        /// </summary>
+        public event EventHandler<string> OnStatusChanged;
+
+        /// <summary>
+        /// Update received data from the serial port to the event subscriber
+        /// </summary>
+        public event EventHandler<string> OnDataReceived;
+
+        /// <summary>
+        /// Update TRUE/FALSE for the serial port connection to the event subscriber
+        /// </summary>
+        public event EventHandler<bool> OnSerialPortOpened;
+
+        /// <summary>
+        /// Return TRUE if the serial port is currently connected
+        /// </summary>
+        public bool IsOpen { get { return _serialPort.IsOpen; } }
+
+        /// <summary>
+        /// Open the serial port connection using basic serial port settings
+        /// </summary>
+        /// <param name="portname">COM1 / COM3 / COM4 / etc.</param>
+        /// <param name="baudrate">0 / 100 / 300 / 600 / 1200 / 2400 / 4800 / 9600 / 14400 / 19200 / 38400 / 56000 / 57600 / 115200 / 128000 / 256000</param>
+        /// <param name="parity">None / Odd / Even / Mark / Space</param>
+        /// <param name="databits">5 / 6 / 7 / 8</param>
+        /// <param name="stopbits">None / One / Two / OnePointFive</param>
+        /// <param name="handshake">None / XOnXOff / RequestToSend / RequestToSendXOnXOff</param>
+        public void Open(
+            string portname = "COM3",
+            int baudrate = 115200,
+            Parity parity = Parity.None,
+            int databits = 8,
+            StopBits stopbits = StopBits.Two,
+            Handshake handshake = Handshake.None)
+        {
+            Close();
+
+            try
+            {
+                _serialPort.PortName = SelectPort(portname);
+                _serialPort.BaudRate = baudrate;
+                _serialPort.Parity = parity;
+                _serialPort.DataBits = databits;
+                _serialPort.StopBits = stopbits;
+                _serialPort.Handshake = handshake;
+
+                _serialPort.ReadTimeout = 50;
+                _serialPort.WriteTimeout = 50;
+
+                _serialPort.Open();
+                StartReading();
+            }
+            catch (IOException)
+            {
+                if (OnStatusChanged != null)
+                    OnStatusChanged(this, string.Format("{0} does not exist.", portname));
+            }
+            catch (UnauthorizedAccessException)
+            {
+                if (OnStatusChanged != null)
+                    OnStatusChanged(this, string.Format("{0} already in use.", portname));
+            }
+            catch (Exception ex)
+            {
+                if (OnStatusChanged != null)
+                    OnStatusChanged(this, "Error: " + ex.Message);
+            }
+
+            if (_serialPort.IsOpen)
+            {
+                string sb = StopBits.None.ToString().Substring(0, 1);
+                switch (_serialPort.StopBits)
+                {
+                    case StopBits.One:
+                        sb = "1"; break;
+                    case StopBits.OnePointFive:
+                        sb = "1.5"; break;
+                    case StopBits.Two:
+                        sb = "2"; break;
+                    default:
+                        break;
+                }
+
+                string p = _serialPort.Parity.ToString().Substring(0, 1);
+                string hs = _serialPort.Handshake == Handshake.None ? "No Handshake" : _serialPort.Handshake.ToString();
+
+                if (OnStatusChanged != null)
+                    OnStatusChanged(this, string.Format(
+                    "Connected to {0}: {1} bps, {2}{3}{4}, {5}.",
+                    _serialPort.PortName,
+                    _serialPort.BaudRate,
+                    _serialPort.DataBits,
+                    p,
+                    sb,
+                    hs));
+
+                if (OnSerialPortOpened != null)
+                    OnSerialPortOpened(this, true);
+            }
+            else
+            {
+                if (OnStatusChanged != null)
+                    OnStatusChanged(this, string.Format(
+                    "{0} already in use.",
+                    portname));
+
+                if (OnSerialPortOpened != null)
+                    OnSerialPortOpened(this, false);
+            }
+        }
+
+        /// <summary>
+        /// Close the serial port connection
+        /// </summary>
+        public void Close()
+        {
+            StopReading();
+            _serialPort.Close();
+
+            if (OnStatusChanged != null)
+                OnStatusChanged(this, "Connection closed.");
+
+            if (OnSerialPortOpened != null)
+                OnSerialPortOpened(this, false);
+        }
+
+        /// <summary>
+        /// Send/write string to the serial port
+        /// </summary>
+        /// <param name="message"></param>
+        public void SendString(string message)
+        {
+            if (_serialPort.IsOpen)
+            {
+                try
+                {
+                    _serialPort.Write(message);
+
+                    if (OnStatusChanged != null)
+                        OnStatusChanged(this, string.Format(
+                        "Message sent: {0}",
+                        message));
+                }
+                catch (Exception ex)
+                {
+                    if (OnStatusChanged != null)
+                        OnStatusChanged(this, string.Format(
+                            "Failed to send string: {0}",
+                            ex.Message));
+                }
+            }
+        }
+
+        private void StartReading()
+        {
+            if (!_keepReading)
+            {
+                _keepReading = true;
+                _readThread = new Thread(ReadPort);
+                _readThread.Start();
+            }
+        }
+
+        private void StopReading()
+        {
+            if (_keepReading)
+            {
+                _keepReading = false;
+                _readThread.Join();
+                _readThread = null;
+            }
+        }
+
+        private void ReadPort()
+        {
+            while (_keepReading)
+            {
+                if (_serialPort.IsOpen)
+                {
+                    try
+                    {
+
+                        string data = _serialPort.ReadLine();
+
+                        if (OnDataReceived != null)
+                            OnDataReceived(this, data);
+                    }
+                    catch (TimeoutException) { }
+                }
+                else
+                {
+                    TimeSpan waitTime = new TimeSpan(0, 0, 0, 0, 50);
+                    Thread.Sleep(waitTime);
+                }
+            }
+        }
+
+        private string SelectPort(string defaultPortName)
+        {
+            string portName;
+
+            Console.WriteLine("Ports disponible:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Veuillez entrer la valeur du port sélectionné (défault: {0}): ", defaultPortName);
+            portName = Console.ReadLine();
+
+            if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+            {
+                portName = defaultPortName;
+            }
+            return portName;
+        }
+
+    }
+}
---
+++ mini-s-desktop/SourisEtcommandes.cs
@@ -0,0 +1,24 @@
+﻿// Projet S5 - Les S de Sherbrooke, tous droits réservés
+// Fichier : SourisEtCommandes.cs
+// Auteur : Hugo Therrien
+// Date : 21 novembre 2017
+//
+// Fichier classe de la souris et d'envoi de commandes Windows
+// 
+
+using System;
+
+namespace mini_s_desktop
+{
+    public class SourisEtCommandes
+    {
+        public SourisEtCommandes()
+        {
+            SerialPortManager.Instance.OnDataReceived += Handler_ReceptionSerie;
+        }
+        private void Handler_ReceptionSerie(object envoyeur, string donnees)
+        {
+            Console.WriteLine("Reçu: {0}", donnees);
+        }
+    }
+}
